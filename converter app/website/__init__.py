c11ba8822f4c6299381b2c815965a010062f6073
OlascoWorks <113733610+OlascoWorks@users.noreply.github.com>
2023-07-30 12:34:11 -0700

    changed secret_key to env_variable..... correctly

--- converter app/website/__init__.py
+++ converter app/website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+import os
 from flask_login import LoginManager
 
 db = SQLAlchemy()
@@ -8,7 +9,7 @@ DB_NAME = 'database.db'
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = SECRET_KEY
+    app.secret_key = os.environ.get('SECRET_KEY')
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
     
